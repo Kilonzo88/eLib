642862650df1f5deb4bd0970fe0c2d59e73e81ad
Dennis <kilonzod19@gmail.com>
2026-04-28 12:10:37 +0300

    Added the BookCard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import { LibraryHero } from '@/components/ui/library-hero'
+import React from 'react';
+import { LibraryHero } from '@/components/ui/library-hero';
+import { sampleBooks } from '@/lib/constants';
+import BookCard from '@/components/ui/BookCard';
 
 const Page = () => {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-12">
       <LibraryHero />
-      {/* This is the Hero section o the app */}
+
+      <section className="max-w-7xl mx-auto px-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pb-16">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </section>
     </div>
   )
 }
