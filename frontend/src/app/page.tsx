ff59fd9f595b0a48fe843a8f74de017578cce376
Dennis <kilonzod19@gmail.com>
2026-04-28 10:38:13 +0300

    Added the Hero section of the app

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { LibraryHero } from '@/components/ui/library-hero'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl font underline'>eLib</h1>
+    <div className="flex flex-col gap-8">
+      <LibraryHero />
+      {/* This is the Hero section o the app */}
     </div>
   )
 }
