05e7faaaf98a5654cfebcf4ba2f67afb3be06df8
Dennis <kilonzod19@gmail.com>
2026-04-29 12:38:21 +0300

    Added guiding text of the page

--- frontend/src/app/(root)/books/new/page.tsx
+++ frontend/src/app/(root)/books/new/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import UploadForm from '@/components/ui/UploadForm'
 
-const page = () => {
+export default function Page() {
   return (
-    <div>page</div>
+    <div className="mx-auto max-w-3xl space-y-10">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-3xl md:text-4xl font-serif font-bold mb-4 text-[var(--primary)]">
+          Add a New Book
+        </h1>
+        <p className="text-[var(--muted-foreground)]">
+          Upload a PDF to generate your interactive interview
+        </p>
+      </section>
+
+      <UploadForm />
+    </div>
   )
 }
-
-export default page
