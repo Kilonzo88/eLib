645702cdcf7cb6606d1c6bf9cb89f0959858517c
Dennis <kilonzod19@gmail.com>
2026-04-29 15:46:05 +0300

    Changed the horizontal container width in which all elements are wrapped in to prevent overstretching the components inside on the screen

--- frontend/src/app/(root)/books/new/page.tsx
+++ frontend/src/app/(root)/books/new/page.tsx
@@ -2,7 +2,7 @@ import UploadForm from '@/components/ui/UploadForm'
 
 export default function Page() {
   return (
-    <div className="mx-auto max-w-3xl space-y-10">
+    <div className="mx-auto max-w-5xl space-y-10">
       <section className="flex flex-col gap-5">
         <h1 className="text-3xl md:text-4xl font-serif font-bold mb-4 text-[var(--primary)]">
           Add a New Book
