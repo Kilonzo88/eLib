c55a4384f845036eec1cf9c3c6da74dd663840d2
Dennis <kilonzod19@gmail.com>
2026-05-04 22:18:20 +0300

    Added getBackendUrl as a drive for clean modular code

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getBackendUrl() {
+  const url = process.env.NEXT_PUBLIC_API_URL;
+  if (!url) {
+    console.warn("NEXT_PUBLIC_API_URL is not defined in .env.local, falling back to localhost:8081");
+    return "http://localhost:8081";
+  }
+  return url;
+}
+
+
