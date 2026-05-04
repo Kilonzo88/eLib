71382ed36f8fa73606e326435dec25fe21e37a3e
Dennis <kilonzod19@gmail.com>
2026-05-04 15:17:03 +0300

    Converted to client component so we can use react hook 'useState'

--- frontend/src/components/ui/BookCard.tsx
+++ frontend/src/components/ui/BookCard.tsx
@@ -1,20 +1,29 @@
-import React from 'react'
+'use client';
+
+import React, { useState } from 'react'
 import { BookCardProps } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const BookCard = ({ title, author, coverURL, slug }: BookCardProps) => {
+    const [imgSrc, setImgSrc] = useState(coverURL);
+    //const fallbackImage = 'https://covers.openlibrary.org/b/id/1-L.jpg';
+
     return (
         <Link href={`/books/${slug}`}>
             <article className="book-card group relative flex flex-col items-center gap-3 transition-all duration-300 hover:-translate-y-2">
                 <figure className="book-card-figure relative">
-                    <div className="book-card-cover-wrapper relative overflow-hidden rounded-xl shadow-[0_10px_20px_rgba(104,81,65,0.15)] transition-all duration-300 group-hover:shadow-[0_20px_35px_rgba(104,81,65,0.25)]">
+                    <div className="book-card-cover-wrapper relative overflow-hidden rounded-xl shadow-[0_10px_20px_rgba(104,81,65,0.15)] transition-all duration-300 group-hover:shadow-[0_20px_35px_rgba(104,81,65,0.25)] bg-[#f8f4e9]">
                         <Image
-                            src={coverURL}
+                            src={imgSrc}
                             alt={title}
                             width={133}
                             height={200}
+                            unoptimized={true}
                             className="book-card-cover h-[200px] w-[133px] object-cover transition-transform duration-500 group-hover:scale-105"
+                        //onError={() => {
+                        //    setImgSrc(fallbackImage);
+                        //}}
                         />
                         <div className="absolute inset-0 bg-primary/5 group-hover:bg-transparent transition-colors duration-300" />
                     </div>
